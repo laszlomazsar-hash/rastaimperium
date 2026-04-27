2e0834d3b38f54b649b29d13acc189fcb55db075
laszlo mazsar <laszlo.mazsar@gmail.com>
2026-04-27 07:16:13 +0100

    Unify L metric computation for unbiased acceptance decisions

---
+++ src/ark_safety/__init__.py
@@ -0,0 +1,7 @@
+from .metric_decision import (
+    MetricPolicy,
+    compute_L_from_snapshots,
+    decide_state_acceptance,
+)
+
+__all__ = ["MetricPolicy", "compute_L_from_snapshots", "decide_state_acceptance"]
