55d12ca6cc568e7c0a70f5bf1c8e03c87aaa3a0c
laszlo mazsar <laszlo.mazsar@gmail.com>
2026-04-27 07:25:56 +0100

    Add safety-manifold-aware derivative estimator

---
+++ src/ark_safety/__init__.py
@@ -0,0 +1,13 @@
+from src.ark_safety.derivatives import (
+    BoxSafetyManifold,
+    DerivativeEstimate,
+    ProbeObservation,
+    SafetyAwareDerivativeEstimator,
+)
+
+__all__ = [
+    "BoxSafetyManifold",
+    "ProbeObservation",
+    "DerivativeEstimate",
+    "SafetyAwareDerivativeEstimator",
+]
