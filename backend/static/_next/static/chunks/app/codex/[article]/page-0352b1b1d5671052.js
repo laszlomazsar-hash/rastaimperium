c95d5462838fc0b309be6875bb4b98a1e79bdbdc
laszlomazsar-hash <256667924+laszlomazsar-hash@users.noreply.github.com>
2026-06-05 10:52:12 -0400

    Restore correct static build with full nav (Empire, Pillars, Codex, Intelligence, Library, Witness, Consulting, Invest)

--- backend/static/_next/static/chunks/app/codex/[article]/page-0352b1b1d5671052.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[353,962],{3478:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,688,23))}},e=>{e.O(0,[688,304,826,358],()=>e(e.s=3478)),_N_E=e.O()}]);
